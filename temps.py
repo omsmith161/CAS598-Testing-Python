1a594b4c0b455708ac38e3da340481e00a8dcc5e
Olivia Smith <116675192+omsmith161@users.noreply.github.com>
2024-11-19 15:04:42 -0700

    Add docstrings and comment to temps.py

--- temps.py
+++ temps.py
@@ -3,13 +3,17 @@ NORMAL_HIGH = 99  # normal human body temp higher end
 
 
 def sort_temperatures(measurements):
+    """Sort temperatures into three categories in comparison to normal human body temperature. 
+    
+    Input is a list of temperatures to sort, outputs are three lists containing low, normal, and high temperatures. 
+    """
     low = []
     normal = []
     high = []
 
     for temperature in measurements:
         if temperature < NORMAL_LOW:
-            low.append(temperature)
+            low.append(temperature)        # list.append() adds the result into the respective list
         elif temperature > NORMAL_HIGH:
             high.append(temperature)
         else:
@@ -19,6 +23,10 @@ def sort_temperatures(measurements):
 
 
 def convert_from_fahrenheit(temps):
+    """Convert temperatures from fahrenheit to celsius. 
+
+    Input is a list of temperatures, output is a list of converted celsius results. 
+    """
     results = []
     for temp in temps:
         results.append((temp-32)/1.8)
@@ -26,6 +34,10 @@ def convert_from_fahrenheit(temps):
     return results
 
 def convert_to_fahrenheit(temps):
+    """Convert temperatures from celsius to fahrenheit. 
+
+    Input is a list of temperatures, output is a list of converted fahrenheit results. 
+    """
     results = []
     for temp in temps:
         results.append(temp*1.8+32)
